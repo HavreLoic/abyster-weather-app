feed1cdca51fc47e7bf6efc189823490d1b57a28
Havre Loic <dongmoabenahavreloic030@gmail.com>
2023-08-12 23:28:44 +0100

    Transmettre les données de manière dynamiques au composant RealTimeWeather. Faire une requête de récupération(fetch request)  pour obtenir la météo actuelle et Faire une requête de récupérer des prévisions météo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+import React, { useState } from 'react';
 import './App.css'
 import Search from './components/search/search';
 import RealTimeWeather from './components/realTimeWeather/realTimeWeather';
 
 function App() {
-  const handleOnSearchChange = (searchData) => { }
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+    const openWeatherApiURL = "https://api.openweathermap.org/data/2.5";
+
+    const fetchCurrentWeather = fetch(`${openWeatherApiURL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric`)
+
+    const fetchWeatherForcast = fetch(`${openWeatherApiURL}/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric`)
+
+    // Agréger le résultat des promesses fetchCurrentWeather et fetchWeatherForcast
+    Promise.all([fetchCurrentWeather, fetchWeatherForcast])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
+      })
+      .catch(console.log);
+
+  }
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      <RealTimeWeather />
+      {currentWeather && <RealTimeWeather data={currentWeather} />}
+      {/* {forecast && <Forecast data={forecast} />} */}
     </div>
   );
 }
--- src/components/realTimeWeather/realTimeWeather.js
+++ src/components/realTimeWeather/realTimeWeather.js
@@ -2,23 +2,24 @@ import React from 'react'
 import { Weather, Flex, TopBottomParagraph } from './realTimeWeather.styled'
 
 
-const RealTimeWeather = () => {
+const RealTimeWeather = ({ data }) => {
+    console.log(data);
     return (
         <Weather>
             <Flex alignitemprop="center">
                 <div>
                     <TopBottomParagraph fontweightprop="600" fontsizeprop="16px" lineheightprop="1" linespacingprop="1px" marginprop="0">
-                        Yaounde
+                        {data.city}
                     </TopBottomParagraph>
 
                     <TopBottomParagraph fontweightprop="400" fontsizeprop="14px" lineheightprop="1" marginprop="0">
-                        Chaud
+                        {data.weather[0].description}
                     </TopBottomParagraph>
                 </div>
                 <img
                     alt="weather"
                     className="weather-icon"
-                    src={`icons/01d.png`}
+                    src={`icons/${data.weather[0].icon}.png`}
                 />
 
             </Flex>
@@ -26,30 +27,30 @@ const RealTimeWeather = () => {
             <Flex alignitemprop="center">
 
                 <TopBottomParagraph fontweightprop="600" fontsizeprop="70px" lineheightprop="1" linespacingprop="-5px" marginprop="10px 0" widthprop="auto">
-                    29°C
+                    {Math.round(data.main.temp)}°C
                 </TopBottomParagraph>
 
                 <div className='details'>
                     <Flex>
                         <span className="parameter-label">Température</span>
                         <span className="parameter-value">
-                            28°C
+                            {Math.round(data.main.feels_like)}°C
                         </span>
                     </Flex>
 
                     <Flex>
                         <span className="parameter-label">Vent</span>
-                        <span className="parameter-value">m/s</span>
+                        <span className="parameter-value">{data.wind.speed} m/s</span>
                     </Flex>
 
                     <Flex>
                         <span className="parameter-label">Humidité</span>
-                        <span className="parameter-value">%</span>
+                        <span className="parameter-value">{data.main.humidity}%</span>
                     </Flex>
 
                     <Flex>
                         <span className="parameter-label">Pression</span>
-                        <span className="parameter-value">hPa</span>
+                        <span className="parameter-value">{data.main.pressure} hPa</span>
                     </Flex>
                 </div>
             </Flex>

58bb99fe093e7a33a8478c5f3e7631a1db50d9dc
Havre Loic <dongmoabenahavreloic030@gmail.com>
2023-08-12 07:09:04 +0100

    Ajoutez le package dotenv pour masquer les clés API, le composant de recherche implémenté avec react-select-async-paginate et implémentez l'API GeoDB Cities pour guider rapidement les utilisateurs vers les éléments qu'ils souhaitent trouver.

---
+++ src/components/search/search.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { AsyncPaginate } from 'react-select-async-paginate'
+import { GeoDBCitiesUrl } from '../../apiSetups'
+import { GeoDBCitiesOptions } from '../../apiSetups'
+
+const Search = ({ onSearchChange }) => {
+    const [search, setSearch] = useState(null)
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData)
+        onSearchChange(searchData)
+    }
+
+    // Valeur du prop loadOptions à passer dans le composant AsyncPaginate. 
+    // L'implémentation de cette fonction fournie par la doc officielle sur le npm montrant comment l'utiliser 
+    // se trouve dans leur codesandboxsur l'utilisation de la loadOptions https://codesandbox.io/s/o75rno2w65?file=/src/loadOptions.ts
+    const loadOptions = async (inputValue) => {
+        const response = await fetch(
+            `${GeoDBCitiesUrl}/cities?namePrefix=${inputValue}`,
+            GeoDBCitiesOptions
+        )
+        const countriesInformation = await response.json()
+        return {
+            options: countriesInformation.data.map((city) => {
+                return {
+                    value: `${city.latitude} ${city.longitude}`,
+                    label: `${city.name}, ${city.countryCode}`,
+                }
+            }),
+        }
+    };
+
+    return (
+        // Documentation sur l'utilisation du composant AsyncPaginate et les options supplémentaires https://www.npmjs.com/package/react-select-async-paginate
+        <AsyncPaginate
+            value={search}
+            onChange={handleOnChange}
+            placeholder="Rechercher la ville"
+            loadOptions={loadOptions}
+            debounceTimeout={600}
+        />
+    )
+}
+
+export default Search

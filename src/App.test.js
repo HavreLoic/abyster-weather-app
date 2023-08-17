de12565450cd079a1d7dba1aee62549e85b9e8b2
Havre Loic <dongmoabenahavreloic030@gmail.com>
2023-08-17 17:38:41 +0100

    code nettoyer

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,6 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-// describe('App', () => {
-//     test('should call on search change', () => { 
-//         const onSearchChange = jest.fn();
-//         const {  } = render(<App onSearchChange={onSearchChange} />)
-//      })
-// })
-
 test('App render correctly', () => {
     render(<App />);
     expect(screen.getByText('Rechercher la ville')).toBeInTheDocument();
